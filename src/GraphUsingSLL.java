5ed067ff2e223892d26363f5cff85733eb90ec52
FeTree <davidjeisenbaum@gmail.com>
2020-04-23 15:10:22 -0700

    Tracking interactions working

--- src/GraphUsingSLL.java
+++ src/GraphUsingSLL.java
@@ -6,6 +6,9 @@ public class GraphUsingSLL {
     int indexCounter = 0;
     boolean undirected = true;
 
+    // Track interactions
+    int davidAndKevinInteractions = 1;
+
     public GraphUsingSLL(int vertexCount, String graphType) {
         if(graphType.equals("directed")) {
             undirected = false;
@@ -21,9 +24,12 @@ public class GraphUsingSLL {
         int vertexOne = indexForName(sourceVertexName);
         int vertexTwo = indexForName(destinationVertexName);
 
-        System.out.println("Vertex 1 ID: " + vertexOne);
-        System.out.println("Vertex 2 ID: " + vertexTwo);
+//        System.out.println("Vertex 1 ID: " + vertexOne);
+//        System.out.println("Vertex 2 ID: " + vertexTwo);
 
+        if(edgeAlreadyExists(vertexOne, vertexTwo)){
+           davidAndKevinInteractions++; //Connection already exists they have met again increase their count
+        }
 
         arrayOfLists.get(vertexOne).adjList = new Node(vertexTwo, arrayOfLists.get(vertexOne).adjList);
         if(undirected) {
@@ -31,12 +37,18 @@ public class GraphUsingSLL {
         }
     }
 
-    public boolean edgeAlreadyExists(Vertex source, Vertex destination) {
-        while (source.adjList.next != null) {
-            if(source.adjList.next.vertexIndex == destination.adjList.vertexIndex){
+    public boolean edgeAlreadyExists(int sourceID, int destinationID) {
+        Vertex source = arrayOfLists.get(sourceID);
+        Vertex destination = arrayOfLists.get(destinationID);
+
+        Vertex current = source;
+        while (current.adjList != null) {
+            if(source.adjList.next == destination.adjList) {
                 return true;
             }
+            current.adjList = current.adjList.next;
         }
+
         return false;
     }
 
@@ -55,7 +67,7 @@ public class GraphUsingSLL {
         for (int i = 0; i < arrayOfLists.size(); i++) {
             System.out.print(arrayOfLists.get(i).name);
             for (Node node = arrayOfLists.get(i).adjList; node != null; node = node.next) {
-                System.out.print("----->" + arrayOfLists.get(node.vertexIndex).name);
+                System.out.print("----> " + arrayOfLists.get(node.vertexIndex).name);
             }
             System.out.println("\n");
         }
@@ -80,14 +92,16 @@ public class GraphUsingSLL {
     }
 
     public static void main(String[] args) {
-        GraphUsingSLL graph = new GraphUsingSLL(3, "undirected");
+        GraphUsingSLL graph = new GraphUsingSLL(3, "directed");
 
         graph.addVertex("David");
         graph.addVertex("Kevin");
-        graph.addVertex("Adam");
 
-        graph.addEdge("David", "Adam");
-        graph.addEdge("David", "Adam");
+        graph.addEdge("David", "Kevin");
+        graph.addEdge("David", "Kevin");
+        graph.addEdge("David", "Kevin");
+        graph.addEdge("Kevin", "David");
+        System.out.println("Interactions between david and Kevin: " + graph.davidAndKevinInteractions);
 
         graph.print();
     }
